ea767bb6ec050506785a38e5471c8b6df6a39e42
sganesh089 <41573564+sganesh089@users.noreply.github.com>
2022-06-07 20:09:53 +1000

    Update opensearch final.py

--- opensearch final.py
+++ opensearch final.py
@@ -2,11 +2,11 @@ import awswrangler as wr
 import boto3 
 import logging
 
-
+## Opensearch link to access
 host= 'https://search-testopen-g23sew4mxkqxpbkvm7mo5wkgoe.us-east-1.es.amazonaws.com/'
 port=443 #change if port is not 443
 username='testuser'
-password= 'G@nesh1987'
+password= '######'
 
 s3_client = boto3.resource('s3').meta.client
 
@@ -26,16 +26,17 @@ def lambda_handler(event, context):
                                           username= username,
                                           password= password )
     
+    ## saving as dataframe with the help of SQL queries
     df2 = wr.opensearch.search_by_sql(
                     client=os_connection,
                     sql_query='SELECT * FROM movies')
-                    
+    ## saving to lambda default library for storing files.                    
     df2.to_csv('/tmp/sorted.csv', index=False)
     #print (df2)
-    
+    ## uploading to S3
     s3_client.upload_file('/tmp/sorted.csv', 'ganeshtestupload', '{}{}'.format('opensearch','.csv'))
         
-        
+    ##sending SNS notfication    
         #s3_client = boto3.resource('s3').meta.client
     s3 = boto3.resource('s3')
     sns = boto3.client('sns')
@@ -49,7 +50,7 @@ def lambda_handler(event, context):
     
     if obj:
         print ('file found')
-        sns.publish(TopicArn='arn:aws:sns:us-east-1:878228182481:ganesh',Message= URL, Subject='Sightdata report urilink:')
+        sns.publish(TopicArn='arn:aws:sns:us-east-1:878228182481:ganesh',Message= URL, Subject='report urilink:')
     
     else:
         print('file not found')
